bf5fa7cd33e002a9ae1cf9d68a1aaee2b74b5a57
Jason-Choi-SKKU <jasonchoi3@naver.com>
2021-03-22 17:08:55 +0900

    init

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize){
+        return super.init({
+            comment : {
+                type : Sequelize.STRING(100),
+                allowNull : false,
+            },
+            created_at : {
+                type : Sequelize.DATE,
+                allowNull : false,
+                defaultValue : Sequelize.NOW
+            },
+        },{
+            sequelize,
+            timestamps : false,
+            underscored : false,
+            modelName : 'User',
+            tableName : 'users',
+            paranoid : false,
+            charset : 'utf8mb4',
+            collate : 'utf8mb4_general_ci',
+        });
+    }
+
+    static associate(db){
+        db.Comment.belongsTo(db.User, {foreignKey : 'commenter' , targetKey : 'id'});
+    }
+}
